c4ca6327c00efd39adead0532ea5a7bcec1eabc2
strick <strick@yak.net>
2019-11-02 10:28:29 -0700

    comments about 1V per octave & our range.

--- make-vfo-table.go
+++ make-vfo-table.go
@@ -7,7 +7,11 @@ import (
 	"math"
 )
 
-// Span means 256 values, like a single-byte analog input value.
+// Span means 256 values, like a single-byte analog input value from A, B, or K.
+// If a span is 5V, and that's 5 octaves, we obey the "1V per octave" rule.
+// Since there are 3 spans added (A, B, and K) we have a 15-octave range,
+// from 8000 Hz down to about 0.25 Hz.
+// TODO: our 16-bit arithmetic gets in the way of counting lower than 0.25 Hz; do longer-precision adds?
 
 const RATE = 9615
 const MAX_FREQ = 8000
