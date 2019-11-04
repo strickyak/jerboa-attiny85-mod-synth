edc2f7da12a91933bf441b4aa9d34c99720a8e3a
strick <strick@yak.net>
2019-11-03 17:49:17 -0800

    Mod rand uses knob K to slow it down.

--- mod-rand/mod-rand.ino
+++ mod-rand/mod-rand.ino
@@ -1,10 +1,20 @@
 #include "/tmp/jerboa/jerboa.h"
 #include "/tmp/jerboa/random.h"
 
+// Make sure it can run max speed even if the Knob doesn't go down to 0.
+// It just needs to go down to 32.
+constexpr word INITIAL = 32;
+
+word wait;
+
 void Setup() {
-	RandomSetup();
+  RandomSetup();
+  wait = INITIAL;
 }
 
 void Loop() {
-  OUT_F(RandomByte());
+  ++wait;
+  if (wait < InK()) return;
+  wait = INITIAL;
+  OutF(RandomByte());
 }
