0763754ea40b1204b1974dfa603e8a230fa07223
strick <strick@yak.net>
2020-02-08 19:01:13 -0800

    Sine wave VCO.

---
+++ mod-vfo-sin/mod-vfo-sin.ino
@@ -0,0 +1,37 @@
+#include "/tmp/jerboa/jerboa.h"
+#include "/tmp/jerboa/generated-vfo-table.h"
+#include "/tmp/jerboa/generated-sin-table.h"
+
+word phase;
+
+void Setup() {}
+
+void Loop() {
+  word offset = word(InA()) + word(InB()) + word(InK());
+  phase += pgm_read_word(VFO_TABLE+offset);
+
+  byte x = 0xFF & (phase >> 6);
+  byte y;
+  switch (phase & 0xC000) {
+  case 0x0000:
+    y = pgm_read_byte(SIN_TABLE + x);
+    OutF(128 + (y>>1));
+    LedOn();
+    break;
+  case 0x4000:
+    y = pgm_read_byte(SIN_TABLE + 255 - x);
+    OutF(128 + (y>>1));
+    LedOn();
+    break;
+  case 0x8000:
+    y = pgm_read_byte(SIN_TABLE + x);
+    OutF(128 - (y>>1));
+    LedOff();
+    break;
+  case 0xC000:
+    y = pgm_read_byte(SIN_TABLE + 255 - x);
+    OutF(128 - (y>>1));
+    LedOff();
+    break;
+  }
+}
